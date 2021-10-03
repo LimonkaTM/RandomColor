c7a66194369310a045538fbd27e1974a82f1c748
LimonkaTM <etuzin@mail.ru>
2021-10-03 22:50:34 +0300

    added timer and refresh

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ function getColor(){
   const green = randomNum();
   const blue = randomNum();
 
-  return changePage(red, green, blue)
+  setInterval(() => document.location.reload(true), 500)
+  
+  return changePage(red, green, blue);
 }
-
 
 function randomNum(){ 
   return Math.floor(Math.random() * 256)
@@ -15,3 +16,5 @@ function changePage(r, g, b){
   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   document.getElementById('paragraphRGB').innerHTML = `rgb(${r}, ${g}, ${b})`;
 }
+
+setTimeout(getColor(), 500);
